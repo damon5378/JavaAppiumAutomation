ff9df15d515761251319ae3ef210b43f884b1b25
damon5378 <iosifov.konstantin@gmail.com>
2021-09-08 12:28:48 +0300

    Test commit

---
+++ src/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void myFirstTest() {
+        int a = 10;
+        int b = 15;
+
+        if (a > b) {
+            System.out.println("This will never happen");
+        } else {
+            System.out.println("This is what will happen");
+        }
+    }
+}
