3efc61cbd7b42b211b87c6c0dde1d2ae07a053f4
damon5378 <iosifov.konstantin@gmail.com>
2021-09-08 16:36:42 +0300

    Test-2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,11 @@
 public class MainClass {
-    public int getLocalNumber() {
-        return 14;
+    // Test-1
+//    public int getLocalNumber() {
+//        return 14;
+//    }
+    int class_number = 20;
+
+    public int getClassNumber() {
+        return this.class_number;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,13 +1,26 @@
 import org.junit.Test;
 
 public class MainClassTest extends MainClass {
+//    @Test
+//    public void testGetLocalNumber() {
+//        int a = this.getLocalNumber();
+//        if (a == 14) {
+//            System.out.println("Method returns: " + a);
+//        } else {
+//            System.out.println("Method returns wrong data");
+//        }
+//
+//    }
+
     @Test
-    public void testGetLocalNumber() {
-        int a = this.getLocalNumber();
-        if (a == 14) {
-            System.out.println("Method returns: " + a);
+    public void testGetClassNumber() {
+        int a = this.getClassNumber();
+        if (a <= 45) {
+            System.out.println("Good");
+        } else if (a >= 45) {
+            System.out.println("Not good");
         } else {
-            System.out.println("Method returns wrong data");
+            System.out.println("Your number from other Universe");
         }
 
     }
