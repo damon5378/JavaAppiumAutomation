23992c6fcf2f503321d484170d7f1084ddb41f88
damon5378 <iosifov.konstantin@gmail.com>
2021-09-08 23:35:50 +0300

    Test-2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,9 @@
 public class MainClass {
 
-    public int getLocalNumber() {
-        return 14;
+    int class_number = 20;
+
+    public int getClassNumber() {
+        return this.class_number;
     }
 
 }
